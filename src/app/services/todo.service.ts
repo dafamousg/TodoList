c1f232cb06568a8f03c0570b84c0268a3c7763b5
ginokalyun <ginokalyun@gmail.com>
2020-09-02 20:47:21 +0200

    Retrieved TODOS from API

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,35 +1,21 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http'
+import { Todo } from '../models/Todo';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  todosUrl:string = 'https://jsonplaceholder.typicode.com/todos';
+  todosLimit:string = '?_limit=5';
 
+  constructor(private http:HttpClient) { }
 
-  getTodos() {
-    return [
-      {
-        id:1,
-        title:"Todo 1",
-        completed: false
-      },
-      {
-        id:2,
-        title:"Todo 2",
-        completed: true
-      },
-      {
-        id:3,
-        title:"Todo 3",
-        completed: true
-      },
-      {
-        id:4,
-        title:"Todo 4",
-        completed: false
-      },
-    ];
+  
+  getTodos():Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
+    
   }
 }
